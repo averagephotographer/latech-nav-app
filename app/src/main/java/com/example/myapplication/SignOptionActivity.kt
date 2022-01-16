762685ecbbf653ca3e414bd9b05cd25f6712f288
jenniferjung1223 <jjung52159@gmail.com>
2022-01-16 14:55:55 -0600

    button working
    you can now press the sign up button and it takes you to the register screen

--- app/src/main/java/com/example/myapplication/SignOptionActivity.kt
+++ app/src/main/java/com/example/myapplication/SignOptionActivity.kt
@@ -6,14 +6,19 @@ import android.os.Bundle
 import android.widget.Button
 
 class SignOptionActivity : AppCompatActivity() {
-    private lateinit var signin_btn : Button
+    private lateinit var signin_btn: Button
+    private lateinit var signup_btn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_option)
         signin_btn = findViewById(R.id.sign_in)
+        signup_btn = findViewById(R.id.sign_up)
 
-        signin_btn.setOnClickListener{
+        signin_btn.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
+        }
+        signup_btn.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
         }
     }
 }
