0002f36a3ab78fb70a36ac67a916b3f736ab58cf
averagephotographer <christopher.tullier98@gmail.com>
2022-01-17 15:12:05 -0600

    Ruston

--- app/src/main/java/com/example/myapplication/MapsActivity.kt
+++ app/src/main/java/com/example/myapplication/MapsActivity.kt
@@ -41,8 +41,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val ruston = LatLng(32.6, -92.6)
+        mMap.addMarker(MarkerOptions().position(ruston).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(ruston))
     }
 }
