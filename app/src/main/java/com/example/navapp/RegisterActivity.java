b5d3425af2059a7d739d65bbe0ab8280dc76d0f7
jenniferjung1223 <jjung52159@gmail.com>
2022-01-29 16:23:09 -0600

    finished migration from kotlin to java!
    Added a confirm password and username as well. the text for "Already have an account" is now clickable and will lead you to the login screen.

--- app/src/main/java/com/example/navapp/RegisterActivity.java
+++ app/src/main/java/com/example/navapp/RegisterActivity.java
@@ -1,4 +1,98 @@
 package com.example.navapp;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class RegisterActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class RegisterActivity extends AppCompatActivity implements TextWatcher {
+    private TextView haveAccount_text;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity);
+        haveAccount_text = (TextView) findViewById(R.id.haveAccount);
+        haveAccount_text.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+            startActivity(intent);
+            }
+        });
+
+        EditText password = (EditText)findViewById(R.id.inputPassword);
+        EditText cpassword = (EditText)findViewById(R.id.inputCPassword);
+        Button loginNow = (Button) findViewById(R.id.buttonRegister);
+        password.addTextChangedListener(this);
+        loginNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!isValidPassword(password.getText().toString())){
+                    Toast.makeText(RegisterActivity.this,"Please input valid combination of password!",Toast.LENGTH_LONG).show();
+                } else if((!password.getText().toString().equals(cpassword.getText().toString()))){
+                    Toast.makeText(RegisterActivity.this, "Confirm Password Not Matched", Toast.LENGTH_SHORT).show();
+                } else if((password.getText().toString().equals(cpassword.getText().toString()))) {
+                    Toast.makeText(RegisterActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void beforeTextChanged(
+            CharSequence s, int start, int count, int after) {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        updatePasswordStrengthView(s.toString());
+    }
+
+    private void updatePasswordStrengthView(String password) {
+
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        TextView strengthView = (TextView) findViewById(R.id.password_strength);
+        if (TextView.VISIBLE != strengthView.getVisibility())
+            return;
+
+        if (password.isEmpty()) {
+            strengthView.setText("");
+            progressBar.setProgress(0);
+            return;
+        }
+        if (isValidPassword(password)){
+            progressBar.setProgress(100);
+            strengthView.setText("Strong");
+        }
+        else {
+            progressBar.setProgress(0);
+            strengthView.setText("Weak");
+        }
+    }
+    public boolean isValidPassword(final String password) {
+        Pattern pattern;
+        Matcher matcher;
+        final String PASSWORD_PATTERN = "^(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+        pattern = Pattern.compile(PASSWORD_PATTERN);
+        matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
 }
