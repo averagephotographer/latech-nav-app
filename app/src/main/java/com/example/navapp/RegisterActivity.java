18a754f0b9e4f2c3e8f98b84867da9afdac0eb3f
sadiatibrahim <57956925+sadiatibrahim@users.noreply.github.com>
2022-03-18 19:42:02 -0500

    users can sign up and it saves into a database

--- app/src/main/java/com/example/navapp/RegisterActivity.java
+++ app/src/main/java/com/example/navapp/RegisterActivity.java
@@ -1,9 +1,11 @@
 package com.example.navapp;
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,8 +13,13 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.mysql.jdbc.Driver;
 
 import java.sql.Connection;
@@ -20,25 +27,27 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-<<<<<<< Updated upstream
-=======
 import javax.xml.transform.Result;
 
-import org.mindrot.jbcrypt.*;
+//import org.mindrot.jbcrypt.*;
 
->>>>>>> Stashed changes
+
 
 public class RegisterActivity extends AppCompatActivity implements TextWatcher {
     private TextView haveAccount_text;
 
     public static final String DATABASE_NAME = "nav-app";
-    public static final String url = "jdbc:mysql://nav-app.czayyymumdfr.us-east-1.rds.amazonaws.com:3306/nav-app";
+    public static final String url = "jdbc:mysql://nav-app.czayyymumdfr.us-east-1.rds.amazonaws.com:3306/nav app";
     public static final String username = "admin", password="latechbulldog";
 
     public static final String TABLE_NAME = "user_profile";
+
+    FirebaseFirestore db;
 
 
 
@@ -47,14 +56,17 @@ public class RegisterActivity extends AppCompatActivity implements TextWatcher {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_activity);
+        db = FirebaseFirestore.getInstance();
+        db.setLoggingEnabled(true);
         haveAccount_text = (TextView) findViewById(R.id.haveAccount);
         haveAccount_text.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-            startActivity(intent);
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
             }
         });
+
         EditText email = (EditText)findViewById(R.id.inputEmail);
         EditText input_username = (EditText)findViewById(R.id.inputUsername);
         EditText password = (EditText)findViewById(R.id.inputPassword);
@@ -68,23 +80,37 @@ public class RegisterActivity extends AppCompatActivity implements TextWatcher {
                 String username_str = input_username.getText().toString();
                 String password_str = password.getText().toString();
 
+                Map<String,Object> user = new HashMap<>();
+                user.put("username", username_str);
+                user.put("email", email_str);
+
                 if(!isValidPassword(password_str)){
                     Toast.makeText(RegisterActivity.this,"Please input valid combination of password!",Toast.LENGTH_LONG).show();
                 } else if((!password_str.equals(cpassword.getText().toString()))){
                     Toast.makeText(RegisterActivity.this, "Confirm Password Not Matched", Toast.LENGTH_SHORT).show();
-<<<<<<< Updated upstream
-                } else {
+                }
+                else  {
+                    user.put("password", password_str);
+
+                    db.collection("user_profile")
+                            .add(user)
+                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                @Override
+                                public void onSuccess(DocumentReference documentReference) {
+                                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                    startActivity(intent);
+
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(RegisterActivity.this, "Login failed", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+
                     //Toast.makeText(RegisterActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                    Toast.makeText(RegisterActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
-=======
-                } else  {
-<<<<<<< HEAD
-                    //Toast.makeText(RegisterActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                    new Task().execute();
-=======
->>>>>>> Stashed changes
-                    addTemp(username_str, password_str, email_str);
->>>>>>> 9f26046295797d0362e44a1f0b3f2843f169890a
+
                 }
             }
         });
@@ -136,30 +162,10 @@ public class RegisterActivity extends AppCompatActivity implements TextWatcher {
     }
 
 
-<<<<<<< Updated upstream
-    public void addTemp(String Username, String hashpassword, String Emaillocalname) {
-        Toast.makeText(RegisterActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
-
-        /*
-        new Thread(() -> {
-            Toast.makeText(RegisterActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
-        */
-        try {
-
-               Toast.makeText(RegisterActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                /*
-                Class.forName("com.mysql.jdbc.Driver");
-                Connection connection = DriverManager.getConnection(url, username, password);
-                Statement statement = connection.createStatement();
-=======
-<<<<<<< HEAD
-    //public static void addTemp(String Username, String hpassword, String Email) {
+    /*
     class Task extends AsyncTask<Void, Void, Void>{
         String records = "";
         String error = "";
-=======
-    public void addTemp(String Username, String hashpassword, String Emaillocalname) {
->>>>>>> 9f26046295797d0362e44a1f0b3f2843f169890a
 
         @Override
         protected Void doInBackground(Void... voids) {
@@ -171,7 +177,6 @@ public class RegisterActivity extends AppCompatActivity implements TextWatcher {
                 while (resultSet.next()) {
                     records += resultSet.getString(1) + " " + resultSet.getString(2);
 
-<<<<<<< HEAD
                 }
             }
             catch(Exception e){
@@ -187,64 +192,12 @@ public class RegisterActivity extends AppCompatActivity implements TextWatcher {
             super.onPostExecute(aVoid);
         }
     }
+     */
 
 
 
 
 }
 
-            /*
-            //char[] hashed = BCrypt.withDefaults().hashToChar(12,hashpassword.toCharArray());
-            //String[] emailparts = Emaillocalname.split("@");
-            emailparts[1] = "@" + emailparts[1];
-=======
-        String hashed = BCrypt.hashpw(hashpassword,BCrypt.gensalt(14));
-        Toast.makeText(RegisterActivity.this, hashed, Toast.LENGTH_SHORT).show();
 
-        String[] emailparts = Emaillocalname.split("@");
-        emailparts[1] = "@" + emailparts[1];
->>>>>>> 9f26046295797d0362e44a1f0b3f2843f169890a
-            Random r = new Random();
-            int uid = r.nextInt(899999999)+100000000;
-            try {
-                Class.forName("com.mysql.jdbc.Driver");
-                Connection connection = DriverManager.getConnection(url, username, password);
-                Statement statement = connection.createStatement();
-                ResultSet RS;
-                RS = statement.executeQuery("select domainid from domainlist where domainname ="+ emailparts[1] + ";");
-                if (RS.first() == false)
-                {
-                    statement.executeQuery("insert into domainlist (domainname) values ("+emailparts[1]+");");
-                    RS = statement.executeQuery("select domainid from domainlist where domainname ="+ emailparts[1] + ";");
-                    Toast.makeText(RegisterActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
-
-                }
-                String domid = RS.getString("DomainId");
-                //Toast.makeText(RegisterActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
->>>>>>> Stashed changes
-
-                statement.execute("INSERT INTO " + TABLE_NAME + "(username, hashpassword, Emaillocalname) VALUES('" + Username + "', '" + hashpassword + "', '" + Emaillocalname + "')");
-
-                connection.close();
-                */
-
-                Toast.makeText(RegisterActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
-
-            }
-        catch (Exception e) {
-                e.printStackTrace();
-            }
-            */
-
-<<<<<<< Updated upstream
-        //}).start();
-
-
-
-
-    }
-}
-=======
-//}
->>>>>>> Stashed changes
 
