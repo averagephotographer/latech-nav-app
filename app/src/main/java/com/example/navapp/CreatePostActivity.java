d5f3c3f30b7953ebd3b71a17ba80f0cc8d967e4c
jenniferjung1223 <jjung52159@gmail.com>
2022-04-01 20:25:07 -0500

    not working yet

--- app/src/main/java/com/example/navapp/CreatePostActivity.java
+++ app/src/main/java/com/example/navapp/CreatePostActivity.java
@@ -2,7 +2,7 @@ package com.example.navapp;
 
 import static com.example.navapp.RegisterActivity.TAG;
 
-import android.app.ProgressDialog;
+import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -10,17 +10,17 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -31,16 +31,20 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -50,11 +54,11 @@ public class CreatePostActivity extends AppCompatActivity {
     EditText write_title;
     ImageView postImage;
     SharedPreferences sharedPreferences;
-    StorageReference storageReference;
+    StorageReference postImageRef;
     FirebaseFirestore firestore;
     FirebaseAuth firebaseAuth;
-    String currentUserID;
-
+    FirebaseUser mUser;
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,14 +71,34 @@ public class CreatePostActivity extends AppCompatActivity {
         postImage = findViewById(R.id.postImage);
         firestore = FirebaseFirestore.getInstance();
         firebaseAuth = FirebaseAuth.getInstance();
-        /*
-        postImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        postImageRef = FirebaseStorage.getInstance().getReference().child("images");
 
-            }
+        ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            // There are no request codes
+                            Intent data = result.getData();
+                            imageUri = data.getData();
+                            postImage.setImageURI(imageUri);
+                        }
+                    }
+                });
+        postImage.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 //Create Intent
+                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                 intent.setType("image/*");
+                 intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+                 someActivityResultLauncher.launch(intent);
+             }
         });
-           */
+
+
+
         create_post.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -85,13 +109,31 @@ public class CreatePostActivity extends AppCompatActivity {
                 String name = sharedPreferences.getString("username", "");
 
                 Map<String,Object> posts = new HashMap<>();
-                //Map<String, Object> myposts = new HashMap<>();
-                posts.put("title", title);
-                posts.put("description", description);
 
                 if(TextUtils.isEmpty(title)) {
                     Toast.makeText(CreatePostActivity.this, "Please write a title before posting!", Toast.LENGTH_LONG).show();
-                } else {
+                }
+                else {
+                    postImageRef.putFile(imageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                            if (task.isSuccessful()) {
+                                postImageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
+                                        Date date = new Date();
+                                        SimpleDateFormat formatter = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+                                        String strDate = formatter.format(date);
+                                        posts.put("title", title);
+                                        posts.put("description", description);
+                                        posts.put("imageURL", uri.toString());
+                                        posts.put("datePost", strDate);
+
+                                    }
+                                });
+                            }
+                        }
+                    });
                     DocumentReference documentReference = firestore.collection("posts").document(name).collection("myposts").document(title);
                     documentReference.set(posts).addOnSuccessListener(new OnSuccessListener<Void>() {
                         @Override
