54db5022d2df39e03f9c96443279cf18be521fd8
chrstnhnry <chrstnhnry@gmail.com>
2022-04-07 21:07:14 -0500

    Add gimbal obs

--- app/src/main/java/com/example/navapp/MapsActivity.java
+++ app/src/main/java/com/example/navapp/MapsActivity.java
@@ -31,7 +31,11 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import com.example.navapp.databinding.ActivityMapsBinding;
+import com.gimbal.android.BeaconSighting;
 import com.gimbal.android.Gimbal;
+import com.gimbal.android.PlaceEventListener;
+import com.gimbal.android.PlaceManager;
+import com.gimbal.android.Visit;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
@@ -76,6 +80,8 @@ public class MapsActivity extends DrawerBaseActivity
         SharedPreferences sharedPreferences;
         public static final String fileName = "login";
         public static final String Username = "username";
+        private static final String GIMBAL_API_KEY = "3f3ef8ff-52f3-46d3-9a8b-d784680b4c85";
+        private PlaceManager placeManager;
 
         /**
          * Flag indicating whether a requested permission has been denied after returning in
@@ -159,6 +165,12 @@ public class MapsActivity extends DrawerBaseActivity
             AutoCompleteTextView textView=(AutoCompleteTextView)findViewById(R.id.txtcountries);
             textView.setThreshold(3);
             textView.setAdapter(adapter);
+
+            Gimbal.setApiKey(this.getApplication(), GIMBAL_API_KEY);
+            setUpGimbalPlaceManager();
+            Gimbal.start();
+
+
 
             oButton.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -269,6 +281,34 @@ public class MapsActivity extends DrawerBaseActivity
                     builder.show();
                 }
             });
+        }
+
+        private void setUpGimbalPlaceManager() {
+
+            PlaceEventListener placeEventListener = new PlaceEventListener() {
+
+                @Override
+                public void onVisitStart(Visit visit) {
+                    super.onVisitStart(visit);
+                    android.util.Log.i("onVisitStart", "overridden");
+                }
+
+                @Override
+                public void onVisitEnd(Visit visit) {
+                    android.util.Log.i("onVisitEnd", "overridden");
+                    super.onVisitEnd(visit);
+                }
+
+                public void onBeaconSighting(BeaconSighting sighting, List<Visit> visits) {
+
+                    android.util.Log.i("oBS: sighting", "" + sighting.toString());
+                    android.util.Log.i("oBS: visits", "" + visits.toString());
+                    // This will be invoked when a beacon assigned to a place within a current visit is sighted.
+                }
+            };
+
+            PlaceManager.getInstance().addListener(placeEventListener);
+
         }
 
         //PLaces markers for services
