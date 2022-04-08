f64eeb8ac361cf21ee1863bc374b49d3ff90abf9
averagephotographer <christopher.tullier98@gmail.com>
2022-04-07 20:57:30 -0500

    this works
    sometimes
    clean build maybe?

--- app/src/main/java/com/example/navapp/BeaconsActivity.java
+++ app/src/main/java/com/example/navapp/BeaconsActivity.java
@@ -1,23 +1,17 @@
 package com.example.navapp;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import com.gimbal.android.Beacon;
 import com.gimbal.android.Gimbal;
-import com.gimbal.android.Communication;
-import com.gimbal.android.CommunicationListener;
-import com.gimbal.android.CommunicationManager;
-import com.gimbal.android.BeaconEventListener;
 import com.gimbal.android.BeaconSighting;
-import com.gimbal.android.BeaconManager;
 
-import com.gimbal.android.Place;
 import com.gimbal.android.PlaceEventListener;
 import com.gimbal.android.PlaceManager;
 import com.gimbal.android.Visit;
-import com.gimbal.location.established.Location;
 
 import java.util.List;
 
@@ -28,14 +22,31 @@ public class BeaconsActivity extends AppCompatActivity {
     private static int r = 0;
     private static final String RSSI = "";
     private PlaceManager placeManager;
+    ListView l;
 
+    String beacon_rssi_list[] = {
+            "Spot 1", "Spot 2",
+            "Spot 3"};
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_beacons);
 
         Gimbal.setApiKey(this.getApplication(), GIMBAL_API_KEY);
         setUpGimbalPlaceManager();
         Gimbal.start(); // note: make sure to get location permission from user before this, see docs
 
+        l = findViewById(R.id.gimbal_information_list);
+        ArrayAdapter<String> arr;
+        arr = new ArrayAdapter<String>(
+                this,
+                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+                beacon_rssi_list);
+        l.setAdapter(arr);
+
+        android.util.Log.i("isStarted", "" + Gimbal.isStarted());
     }
 
     private void setUpGimbalPlaceManager() {
@@ -55,6 +66,7 @@ public class BeaconsActivity extends AppCompatActivity {
             }
 
             public void onBeaconSighting(BeaconSighting sighting, List<Visit> visits) {
+
                 android.util.Log.i("oBS: sighting", "" + sighting.toString());
                 android.util.Log.i("oBS: visits", "" + visits.toString());
                 // This will be invoked when a beacon assigned to a place within a current visit is sighted.
