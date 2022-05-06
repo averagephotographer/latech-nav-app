17d156c1128530d95c6ee95000ad12d029743da6
the-newdeal <alex@hagdav.com>
2022-05-05 19:45:36 -0500

    presentation optimizaiton

--- app/src/main/java/com/example/navapp/LoginActivity.java
+++ app/src/main/java/com/example/navapp/LoginActivity.java
@@ -124,7 +124,7 @@ public class LoginActivity extends AppCompatActivity {
         dontHaveAcc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                Intent intent = new Intent(LoginActivity.this, SignOptionsActivity.class);
                 startActivity(intent);
             }
         });
